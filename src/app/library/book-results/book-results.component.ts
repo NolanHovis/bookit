0085771a36b3d363926f4b07b9fc7b9ce77a7b39
Nolan Hovis <nolan.hovisPersonal@gmail.com>
2023-10-05 12:51:54 -0500

    Extra stuff from class 1

--- src/app/library/book-results/book-results.component.ts
+++ src/app/library/book-results/book-results.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/shared/book.model';
 
 @Component({
   selector: 'app-book-results',
   templateUrl: './book-results.component.html',
-  styleUrls: ['./book-results.component.css']
+  styleUrls: ['./book-results.component.css'],
 })
 export class BookResultsComponent implements OnInit {
+  allBooks: Book[] = [
+    new Book(
+      'Book of Testing',
+      'Will Wilder',
+      'Mystery',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
